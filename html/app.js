3f882fa1916e1923d59e795edada0b062b858f6b
james king <jamesasuelimen77@gmail.com>
2023-04-06 19:59:19 +0100

    initial commit

---
+++ html/app.js
@@ -0,0 +1,33 @@
+
+
+let body = querySelector("body");
+
+ body.style.width = "100vw";
+ body.style.height = "100vh";
+ body.style.display = "flex";
+ body.style.marginTop = "10%";
+
+const root = document.querySelector("#root");
+root.style.backgroundColor = "yellow ";
+root.style.height = "100vh";
+root.style.width = "100vw";
+root.backGroundImage = "linear-gradient(to right, # 862e96, #e34881)";
+
+root.style.borderRadius = "8px";
+root,style.height ="50%";
+root.style.width = "50%";
+root.style.textAlign = "center";
+root. style.padding = "1% 0% 3% 0%";
+
+
+let rootInnerPtag = <p class = "ptag"> Ui Card Design</p>;
+root.innerHTML = rootInnerPtag;
+let ptag = document.querySelector(".ptag");
+
+ptag.style.color = "#1dcf9";
+ptag.style.fontSize = "30px";
+ 
+
+
+
+ 
---
+++ html/guessingGame.js
@@ -0,0 +1,25 @@
+ 
+let input = Number(prompt("please enter a number: "))
+ 
+ let guessingGame = function (input){ 
+        let random = Math.floor((Math.random() * 100) + 1);
+       
+        if(input == random){
+            alert(`congratulation you win!!!, the lucky number is  ${random}`);  
+        }
+    
+        alert(`try again, the lucky number is ${random}`);
+    }
+
+   
+     let count = 0;
+    do{
+      input(guessingGame(input))
+      count++;
+      if(count == 5){
+        alert("game over!!!")
+        break;
+      }
+    }while(count < 5)
+
+ 
