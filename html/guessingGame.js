68ff7292af957d914283d3c479dc29491e7acf39
james king <jamesasuelimen77@gmail.com>
2023-04-13 12:40:19 +0100

    initial commit

--- html/guessingGame.js
+++ html/guessingGame.js
@@ -1,25 +1,34 @@
  
-let input = Number(prompt("please enter a number: "))
+
+
+
+// var play = document.getElementById("play").click;
+
+
+
+// let input = Number(prompt("please enter a number: "))
  
- let guessingGame = function (input){ 
-        let random = Math.floor((Math.random() * 100) + 1);
+//  let guessingGame = function (input){ 
+//         let random = Math.floor((Math.random() * 100) + 1);
        
-        if(input == random){
-            alert(`congratulation you win!!!, the lucky number is  ${random}`);  
-        }
+//         if(input == random){
+//             alert(`congratulation you win!!!, the lucky number is  ${random}`);  
+//         }
     
-        alert(`try again, the lucky number is ${random}`);
-    }
+//         alert(`try again, the lucky number is ${random}`);
+//     }
 
    
-     let count = 0;
-    do{
-      input(guessingGame(input))
-      count++;
-      if(count == 5){
-        alert("game over!!!")
-        break;
-      }
-    }while(count < 5)
+//      let count = 0;
 
+    
+//     do{
+//       input(guessingGame(input))
+//       count++;
+//       if(count == 5){
+//         alert("game over!!!")
+//         break;
+//       }
+//     }while(count < 5)
+  
  
